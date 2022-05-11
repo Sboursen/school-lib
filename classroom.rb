58079b454d543669c6134b82f36065ed2060f56d
sboursen <dev.sboursen@gmail.com>
2022-05-11 11:30:36 +0100

    Create one to many relationsip between Classroom and Student

--- classroom.rb
+++ classroom.rb
@@ -1,7 +1,17 @@
+$LOAD_PATH << '.'
+require 'student'
+
 class Classroom
   attr_accessor :label
+  attr_reader :students
 
-  def intialize(label)
+  def initialize(label)
     @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students.push(student) unless @students.include?(student)
+    student.classroom = self
   end
 end
--- student.rb
+++ student.rb
@@ -2,11 +2,17 @@ $LOAD_PATH << '.'
 require 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
