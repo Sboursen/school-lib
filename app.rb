b6d614fe1acc19a108ac1cf71faecf0ef619727f
sboursen <dev.sboursen@gmail.com>
2022-05-12 11:43:10 +0100

    Create display_for_user methode

--- app.rb
+++ app.rb
@@ -7,8 +7,12 @@ require 'book'
 require 'rental'
 
 class App
+  attr_accessor :user_input
+
   def initialize
     puts 'Welcome to School Library App!'
+    print_prompt
+    @user_input = gets.chomp
   end
 
   def print_prompt
@@ -23,11 +27,42 @@ class App
     puts ''
     puts 'Select an option: '
   end
+
+  def user_input_valid?(user_input)
+    %w[1 2 3 4 5 6 7].include?(user_input)
+  end
+
+  def display_for_user(user_input)
+    case user_input
+    when '1'
+      list_all_books
+    when '2'
+      list_all_people
+    when '6'
+      list_all_rentals
+  end
+
+  def run
+    case user_input
+    when '1', '2', '6'
+      display_for_user(user_input)
+    when '3', '4', '5'
+      create_for_user
+    when '7'
+      exit(true)
+    else
+      puts "\nInvalid input \"#{user_input}\" !"
+      puts 'please try with one of these options:'
+      print_prompt
+      @user_input = gets.chomp
+      run
+    end
+  end
 end
 
 def main
   app = App.new
-  app.print_prompt
+  app.run
 end
 
 main
