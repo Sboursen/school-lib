368145fbe023d63eaecc310fc66f4d1631ac3234
Awais Amjed <awaisamjed171@gmail.com>
2022-05-17 20:43:37 +0500

    Add hashing functionality in book class

--- book.rb
+++ book.rb
@@ -12,4 +12,16 @@ class Book
     @rentals.push(rental) unless @rentals.include?(rental)
     rental.book = self
   end
+
+  def to_hash
+    hash = {}
+    hash[:class] = self.class
+    hash[:title] = @title
+    hash[:author] = @author
+    hash
+  end
+
+  def self.create_from_hash(hash)
+    Book.new(hash['title'], hash['author'])
+  end
 end
