92a60f2f5c54ec9292ef488be9faf90d69434010
sboursen <dev.sboursen@gmail.com>
2022-05-17 13:56:02 +0100

    Use the JsonHandler module inside the DataStorage Class

--- data_storage.rb
+++ data_storage.rb
@@ -1,15 +1,15 @@
+require 'json_handler'
+
 class DataStorage
   def initialize(file_name)
     @file_name = file_name
   end
 
   def read_data
-    if File.exist?(@file_name)
-      File.read(@file_name).split
-    end
+    return JsonHandler.parse_json(File.read(@file_name)) if File.exist?(@file_name)
   end
 
   def write_data(data)
-    File.write(@file_name, data)
+    File.write(@file_name, JsonHandler.generate_json(data))
   end
 end
